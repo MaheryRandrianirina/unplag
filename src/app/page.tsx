ccdb8b4c77a818c5e216553c699b003a2d69947c
MaheryRandrianirina <maheryrandrianirina2000@gmail.com>
2025-06-03 17:14:26 +0300

    feat: page ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
+import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
   return (
     <div>
-      Hello, welcome to unplag ! So ?
+      <main>
+        <div className="header">
+          <h1>Welcome to Unplag !</h1>
+          <p>Fight plagiarism easily.</p>
+          <p>Unplag is a tool designed to help you detect and prevent plagiarism in your work.</p>
+        </div>
+        <div className="button-wrapper">
+          <button className="button-64" role="button"><span className="text">Upload your file</span></button>
+        </div>
+      </main>
     </div>
   );
 }
