8c9a262ba323040bc03f34b27525ae20156f468c
MaheryRandrianirina <maheryrandrianirina2000@gmail.com>
2025-06-12 12:54:39 +0300

    feat(search): add google search using serpapi

--- src/lib/Plagiarism.ts
+++ src/lib/Plagiarism.ts
@@ -1,14 +1,30 @@
+import { GoogleSearchResult } from "../../types/googleSearch";
 import { FileOperator } from "./FileOperator";
 import { NLP } from "./NLP";
+import { googleSearch } from "./utils";
 
 export class Plagiarism {
+
     async check(file: File) {
         const fo = new FileOperator(file);
         await fo.store();
         const randomPhrases = await fo.extractRandomPhrases();
         
         const nlp = new NLP(randomPhrases);
-        const lemmaPhrases = nlp.prepare();
-        console.log(lemmaPhrases)
+        const phrases = nlp.prepare();
+        
+        const results = this.#search(phrases)
+    }
+
+    /**
+     * google search
+     * 
+     * @param phrases 
+     */
+    async #search(phrases: string[]) {
+        const results: null|GoogleSearchResult[][] = await Promise.all(phrases.map(sentence => googleSearch(sentence)));
+        //const results = await Promise.all([googleSearch(phrases[0])]) as GoogleSearchResult[]; // for testing purposes
+
+        return results;
     }
 }
